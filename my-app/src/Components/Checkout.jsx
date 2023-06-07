f0807590abf4a266647ecde5a1f486acc419bca6
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2023-06-07 13:56:42 +0530

    Feat : Added CheckOut

---
+++ my-app/src/Components/Checkout.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { DataContext } from "../Context/DataContext";
+
+const Checkout = () => {
+  const {
+    state: { cart },
+  } = useContext(DataContext);
+
+  let valueCart = cart?.reduce(
+    (acc, item) => {
+      return { ...acc, total: acc.total + item.price };
+    },
+    { total: 0 }
+  );
+  let OrignalvalueCart = cart?.reduce(
+    (acc, item) => {
+      return { ...acc, total: acc.total + item.orignalPrice };
+    },
+    { total: 0 }
+  );
+  console.log(valueCart);
+  console.log(OrignalvalueCart);
+  return (
+    <div className="CheckOutContainer">
+      <h4>M.R.P : {OrignalvalueCart.total}</h4>
+      <h4>Discount : - {OrignalvalueCart.total - valueCart.total} </h4>
+      <h4>Total Cart Value : {valueCart.total}</h4>
+      <button>CheckOut</button>
+    </div>
+  );
+};
+
+export default Checkout;
--- my-app/src/Components/Signin.jsx
+++ my-app/src/Components/Signin.jsx
@@ -1,43 +1,35 @@
-import React, { useState } from "react";
-
+import React, { useContext } from "react";
+import { DataContext } from "../Context/DataContext";
 export const Signin = () => {
-  const [signInData, setSignInData] = useState({ email: "", password: "" });
-  const handelSubmit = (e) => {
-    e.preventDefault();
+  const { isLoggedIn, handleLogin } = useContext(DataContext);
+  // const [signInData, setSignInData] = useState({ email: "", password: "" });
 
-    setSignInData({ email: "", password: "" });
-  };
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  };
+  // const handelSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   setSignInData({ email: "", password: "" });
+  // };
+  // const handleChange = (e) => {
+  //   console.log(e.target.value);
+  // };
+
   return (
     <div className="signIn">
       <h2> I have already an account </h2>
       <span>Sign i with your Email and Password</span>
 
-      <form onSubmit={handelSubmit}>
-        <input
-          type="email"
-          name="email"
-          onChange={handleChange}
-          value={signInData.email.value}
-          required
-        />
-        <label>Email</label>
-        <input
-          type="password"
-          name="password"
-          value={signInData.password.value}
-          onChange={handleChange}
-          required
-        />
+      <input type="email" name="email" required />
+      <label>Email</label>
+      <input type="password" name="password" required />
 
-        <label>Password</label>
-        <input type="submit" value="submit form" />
+      <label>Password</label>
+      <input type="submit" value="submit form" />
 
-        <p>{signInData.email.valueOf}</p>
-        <p>{signInData.password.valueOf}</p>
-      </form>
+      <button onClick={() => handleLogin(!isLoggedIn)}>
+        {!isLoggedIn ? "Login Guest" : "Logout"}
+      </button>
+      {console.log(isLoggedIn)}
+      {/* </form> */}
     </div>
   );
 };
