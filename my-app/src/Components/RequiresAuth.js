f9d809196900e2f238a671d1f627e675778bab90
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2023-06-07 14:20:26 +0530

    Feat : Added Auth for Cart

---
+++ my-app/src/Components/RequiresAuth.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { DataContext } from "../Context/DataContext";
+
+export function RequiresAuth({ children }) {
+  const { isLoggedIn } = useContext(DataContext);
+  const location = useLocation();
+  return isLoggedIn ? (
+    children
+  ) : (
+    <Navigate to="/Signin" state={{ from: location }} />
+  );
+}
