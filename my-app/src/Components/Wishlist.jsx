a33ad0cc7b08108267b37715c8aa7b243c9c96f0
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2023-06-07 08:20:47 +0530

    Feat : wishList working

--- my-app/src/Components/Wishlist.jsx
+++ my-app/src/Components/Wishlist.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DataContext } from "../Context/DataContext";
 
 export const Wishlist = () => {
+  const {
+    state: { wishlist },
+    handleRemoveWishlist,
+  } = useContext(DataContext);
   return (
-    <div>
-      <p>Wishlist</p>
+    <div
+      className="container"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      {wishlist?.map((item) => {
+        return (
+          <div className="WishlistContainer">
+            <div className="imgContainer">
+              <div key={item.id}>
+                <img
+                  src={item.imageURL}
+                  alt="productImage"
+                  className="imgclass"
+                />
+                <h2 style={{ margin: "0" }}>{item.Name}</h2>
+                <p style={{ margin: "0" }}>
+                  <b>{item.price} Rs</b>
+                </p>
+                <button onClick={() => handleRemoveWishlist(item)}>
+                  {" "}
+                  Remove{" "}
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
